a722bc4ea3f0bc60213fcb6f642da0e0c37a09ae
Yegor Gursky <yegorgursky@Yegors-MacBook-Air.local>
2021-10-13 20:02:51 +0300

    create project, first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import {Header} from "./components/Header/Header";
+import {About} from "./components/About/About";
+import {Projects} from "./components/Projects/Projects";
+import {Skills} from "./components/Skills/Skills";
+import {Contacts} from "./components/Contacts/Contacts";
+import {Footer} from "./components/Footer/Footer";
+
+
+function App() {
+    return (
+        <div className="App">
+            <Header/>
+            <About/>
+            {/*<Projects/>*/}
+            <Skills/>
+            <Contacts/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default App;
