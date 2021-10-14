3e4d3e0b121b6cc0e777cb72b1d7fd8bac97adf0
Yegor Gursky <yegorgursky@Yegors-MacBook-Air.local>
2021-10-14 21:51:17 +0300

    refactoring layout about, contacts, footer

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,43 +10,38 @@ export const About = () => {
                 <h2 className={style.sectionTitle}>About Me</h2>
                 <div className={style.animatedBar}>/</div>
             </div>
-                <div className={style.infoSection}>
-                    <div className={style.infoImg}>
-                        <img className={style.img} src="" alt="About Picture"/>
+            <div className={style.infoSection}>
+                <div className={style.infoImg}>
+                    <img className={style.img} src="" alt="About Picture"/>
+                </div>
+                <div className={style.contentInfo}>
+                    <div className={style.contentBlock}>
+                        <h2 className={style.contentSubtitle}>Who am i?</h2>
+                        <h6 className={style.contentTitle}>I'm Yegor Gursky, a Frontend Developer</h6>
+                        <div className={style.contentDescription}>
+                            <p>I am a freelancer based in the Belarus. </p>
+                        </div>
                     </div>
-                    <div className={style.contentInfo}>
-                        <div className={style.contentBlock}>
-                            <h2 className={style.contentSubtitle}>Who am i?</h2>
-                            <h6 className={style.contentTitle}>I'm Yegor Gursky, a Frontend Developer</h6>
-                            <div className={style.contentDescription}>
-                                <p>I am a freelancer based in the Belarus. </p>
+                    <div className={style.contentContact}>
+                        <address className={style.contactInfo}>
+                                <div className={style.singleInfo}>
+                                    <span>Name:</span>
+                                    <p>Yegor Gursky</p>
+                                </div>
+                                <div className={style.singleInfo}><span>Age:</span>
+                                    <p>29</p>
                             </div>
-                        </div>
-                        <div className={style.contentContact}>
-                            <address className={style.contactInfo}>
-                                <div className={style.row}>
-                                    <div className={style.singleInfo}>
-                                        <span>Name:</span>
-                                        <p>Yegor Gursky</p>
-                                    </div>
-                                    <div className={style.singleInfo}><span>Age:</span>
-                                        <p>29</p>
-                                    </div>
+                                <div className={style.singleInfo}>
+                                    <span>Email:</span>
+                                    <p><a href="mailto:gurskygursky@example.com">gurskygursky@example.com</a></p>
                                 </div>
-                                <div className={style.row}>
-
-                                    <div className={style.singleInfo}>
-                                        <span>Email:</span>
-                                        <p><a href="mailto:gurskygursky@example.com">gurskygursky@example.com</a></p>
-                                    </div>
-                                    <div className={style.singleInfo}><span>From:</span>
-                                        <p>Minsk, Belarus</p>
-                                    </div>
+                                <div className={style.singleInfo}><span>From:</span>
+                                    <p>Minsk, Belarus</p>
                                 </div>
-                            </address>
-                        </div>
+                        </address>
                     </div>
                 </div>
+            </div>
         </div>
     );
 }
--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -4,17 +4,71 @@ import style from './Contacts.module.css';
 export const Contacts = () => {
     return (
         <div className={style.container}>
-            <div className={style.header}>
-                <h2>Contact</h2>
+            <div className={style.sectionHeader}>
+                <p>Feel free to contact me anytimes</p>
+                <h2>Get in Touch</h2>
             </div>
-            <form>
-
-                <input/><br/>
-                <input/><br/>
-                <textarea/><br/>
-            </form>
-            <button className={style.button}>send </button>
+            <div className={style.sectionContactsForm}>
+                <form className={style.messageForm}>
+                    <h4>Message Me</h4>
+                    <input className={style.input} type={'text'} placeholder={'Name'}/><br/>
+                    <input className={style.input} type={'email'} placeholder={'Email'}/><br/>
+                    <input className={style.input} type={'text'} placeholder={'Subject'}/><br/>
+                    <textarea className={style.textarea} placeholder={'Message'}/><br/>
+                    <button className={style.button}>Send Message</button>
+                </form>
+                <div className={style.sectionContactsInfo}>
+                    <h4>Contact Info</h4>
+                    <p>Always available for freelance<br/>
+                        work if the right project comes along,<br/>
+                        Feel free to contact me!<br/>
+                    </p>
+                    <ul>
+                        <li>
+                            <div>
+                                <span>icon</span>
+                                <div>
+                                    <h6>Name</h6>
+                                    <span>Yegor Gursky</span>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <span>icon</span>
+                                <div>
+                                    <h6>Location</h6>
+                                    <span>Minsk, Belarus</span>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <span>icon</span>
+                                <div>
+                                    <h6>Call Me</h6>
+                                    <span>Minsk, Belarus</span>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <span>icon</span>
+                                <div>
+                                    <h6>Email Me</h6>
+                                    <span>
+                                        <a href="mailto:gurskygursky@example.com">
+                                            gurskygursky@example.com
+                                        </a>
+                                    </span>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
+
     );
 }
 
