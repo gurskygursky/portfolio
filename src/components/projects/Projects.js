fe923f7347a4649cba9dd14f59ddb255db9e2efa
gurskygursky <yahorhurski@gmail.com>
2021-10-22 00:11:03 +0300

    create assets and image folder, add image, refactroing layout projects and project components

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,8 +2,16 @@ import React from "react";
 import style from './Projects.module.css';
 import {Header} from "../../common/header/Header";
 import {Project} from "./project/Project";
+import social from "../../assets/image/Social-Networking-Management-Tools.png"
+import todo from "../../assets/image/15.png"
 
 export const Projects = () => {
+    const divStyleImageSocialNetwork = {
+        backgroundImage: `url(${social})`,
+    };
+    const divStyleImageTodo = {
+        backgroundImage: `url(${todo})`,
+    };
     return (
         <div className={style.container}>
             <Header description={"Showcasing some of my best work"}
@@ -12,11 +20,13 @@ export const Projects = () => {
             <div className={style.containerProjects}>
                 <Project description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit.\n" +
                 "                            Dolore, porro rem quod illo quam, eum alias id, repellendus magni, quas."}
-                         title={"My first React project"}
+                         title={"Social Network"}
+                         style={divStyleImageSocialNetwork}
                 />
                 <Project description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit.\n" +
                 "                            Dolore, porro rem quod illo quam, eum alias id, repellendus magni, quas."}
-                         title={"My first React project"}
+                         title={"Todolist"}
+                         style={divStyleImageTodo}
                 />
             </div>
         </div>
--- src/components/projects/project/Project.js
+++ src/components/projects/project/Project.js
@@ -3,18 +3,14 @@ import style from './Project.module.css';
 
 export const Project = (props) => {
     return (
-            <div className={style.container}>
-                <div className={style.contentImg}><a className={style.img} href="#0">
-                    {/*<img className={style.imgCard} src="img/post-1.jpg" alt="Blog post"/>*/}
-                    {/*<span className={style.contentDate}>11 Dec, 20</span>*/}
-                </a>
-                    <div className={style.contentPost}>
-                        <a href="#0">
-                            <h5 className={style.contentTitle}>{props.title}</h5>
-                        </a>
-                        <p className={style.contentDescription}>{props.description}</p>
-                    </div>
-                </div>
+        <div className={style.container}>
+            <div className={style.contentImg} style={props.style}>
+                {/*<a className={style.button}>Click Me!</a>*/}
             </div>
+            <div className={style.contentProjectInfo} >
+                    <h5 className={style.contentTitle}>{props.title}</h5>
+                <p className={style.contentDescription}>{props.description}</p>
+            </div>
+        </div>
     );
 }
